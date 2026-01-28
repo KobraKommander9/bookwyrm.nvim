67d742d61b14e8a8cb03d88caab9cea5db4ec286
Ed <ed.kilgore@tcnbroadcasting.com>
2026-01-28 09:26:30 -0500

    optimize parser

--- lua/bookwyrm/parser.lua
+++ lua/bookwyrm/parser.lua
@@ -1,48 +1,8 @@
 local M = {}
 
---- Parses metadata from the top of the note.
----
---- @param lines string[] # The buffer lines
---- @param data BookwyrmNote # The note to populate
-local function parse_metadata(lines, data)
-	if lines[1] ~= "---" then
-		return
-	end
-
-	for i = 2, #lines do
-		local line = lines[i]
-		if line == "---" then
-			break
-		end
-
-		local title = line:match("^title:%s*(.+)$")
-		if title then
-			data.title = vim.trim(title):gsub("^([\"'])(.*)%1$", "%2")
-		end
-
-		local aliases = line:match("^alias:%s*%[?(.+)%]?(.*)$") or line:match("^aliases:%s*%[?(.+)%]?(.*)$")
-		if aliases then
-			for a in aliases:gmatch("([^,]+)") do
-				table.insert(data.aliases, {
-					alias = vim.trim(a):gsub("^([\"'])(.*)%1$", "%2"),
-				})
-			end
-		end
-
-		local tags = line:match("^tags:%s*%[?(.+)%]?(.*)$")
-		if tags then
-			for t in tags:gmatch("([^,]+)") do
-				table.insert(data.tags, {
-					tag = vim.trim(t):gsub("^#", ""),
-				})
-			end
-		end
-	end
-end
-
---- Parses anchors from the given buffer's lines and appends them to the
---- data. Anchors can be of 3 main formats, with the ids consisting of
---- alphanumeric, hyphen, or undescore characters:
+--- Parses anchors from the given line and appends them to the data. Anchors
+--- can be of 3 main formats, with the ids consisting of alphanumeric, hyphen,
+--- or undescore characters:
 ---   - Multiline range: \[^start:anchor-id\] ... \[^end:anchor-id\]
 ---   - Span: \[Content\]^anchor-id
 ---   - Block: ^anchor-id
@@ -51,146 +11,160 @@ end
 ---   or line if not found, up until the anchor itself.
 ---
 --- @param bufnr integer # The buffer number
---- @param lines string[] # The buffer lines
+--- @param linenr integer # The line number (0 indexed)
+--- @param line string # The line to parse
+--- @param active_starts table # The list of currently active ranges
 --- @param data BookwyrmNote # The note to populate
-local function parse_anchors(bufnr, lines, data)
-	local active_starts = {}
+local function parse_anchors(bufnr, linenr, line, active_starts, data)
+	-- multiline range anchors
+	line = line:gsub("()%[%^start:([%w%-_]+)%]()", function(start_col, id, end_col)
+		active_starts[id] = {
+			row = linenr,
+			col = start_col - 1,
+			end_col = end_col - 1,
+		}
 
-	for i, line in ipairs(lines) do
-		local row = i - 1
+		return string.rep(" ", end_col - start_col)
+	end)
 
-		for start_col, id, end_col in line:gmatch("()%[%^start:([%w%-_]+)%]()") do
-			active_starts[id] = {
-				row = row,
-				col = start_col - 1,
-				end_col = end_col - 1,
-			}
-		end
+	line = line:gsub("()%[%^end:([%w%-_]+)%]()", function(start_col, id, end_col)
+		local start_data = active_starts[id]
+		if start_data then
+			local lines =
+				vim.api.nvim_buf_get_text(bufnr, start_data.row, start_data.end_col, linenr, start_col - 1, {})
 
-		for start_col, id, end_col in line:gmatch("()%[%^end:([%w%-_]+)%]()") do
-			local start_data = active_starts[id]
-			if start_data then
-				local lines_content =
-					vim.api.nvim_buf_get_text(bufnr, start_data.row, start_data.end_col, row, start_col - 1, {})
-
-				table.insert(data.anchors, {
-					anchor_id = id,
-					content = table.concat(lines_content, "\n"),
-					loc = {
-						start = { line = start_data.row, character = start_data.col },
-						finish = { line = row, character = end_col - 1 },
-					},
-				})
-
-				active_starts[id] = nil
-			end
-		end
-	end
-
-	for i, line in ipairs(lines) do
-		local row = i - 1
-
-		-- mask extracted range anchors
-		local temp_line = line:gsub("%[%^start:[%w%-_]+%]", function(m)
-			return string.rep(" ", #m)
-		end)
-
-		temp_line = temp_line:gsub("%[%^end:[%w%-_]+%]", function(m)
-			return string.rep(" ", #m)
-		end)
-
-		-- basic span: [...]^id
-		temp_line = temp_line:gsub("()(%b[])%^([%w%-_]+)()", function(start_col, content, id, end_col)
 			table.insert(data.anchors, {
 				anchor_id = id,
-				content = content:sub(2, -2), -- strip [ and ]
+				content = table.concat(lines, "\n"),
 				loc = {
-					start = { line = row, character = start_col - 1 },
-					finish = { line = row, character = end_col - 1 },
+					start = { line = start_data.row, character = start_data.col },
+					finish = { line = linenr, character = end_col - 1 },
 				},
 			})
 
-			-- return a string of spaces equal to the length of the entire match
-			return string.rep(" ", end_col - start_col)
-		end)
+			active_starts[id] = nil
+		end
 
-		-- block anchor: <beginning of block>^id
-		for start_col, id, end_col in temp_line:gmatch("()%s%^([%w%-_]+)()") do
-			local final_start = { line = row, character = 0 }
-			local block_text = temp_line:sub(1, start_col - 1)
+		return string.rep(" ", end_col - start_col)
+	end)
 
-			local node = vim.treesitter.get_node({ bufnr = bufnr, pos = { row, start_col - 1 } })
-			if node then
-				--- @type TSNode?
-				local curr = node
+	-- span anchors: [...]^id
+	line = line:gsub("()(%b[])%^([%w%-_]+)()", function(start_col, content, id, end_col)
+		table.insert(data.anchors, {
+			anchor_id = id,
+			content = content:sub(2, -2), -- strip [ and ]
+			loc = {
+				start = { line = linenr, character = start_col - 1 },
+				finish = { line = linenr, character = end_col - 1 },
+			},
+		})
 
-				while curr do
-					local type = curr:type()
+		return string.rep(" ", end_col - start_col)
+	end)
 
-					if type:find("paragraph") or type:find("section") or type:find("list_item") then
-						local s_row, s_col, _, _ = curr:range()
-						final_start = { line = s_row, character = s_col }
+	-- block anchor: <beginning of block>^id
+	for start_col, id, end_col in line:gmatch("()%s%^([%w%-_]+)()") do
+		local final_start = { line = linenr, character = 0 }
+		local block_text = line:sub(1, start_col - 1)
 
-						local content_lines = {}
-						if not (s_row == row and s_col == start_col - 1) then
-							content_lines = vim.api.nvim_buf_get_text(bufnr, s_row, s_col, row, start_col - 1, {})
-						end
+		local node = vim.treesitter.get_node({ bufnr = bufnr, pos = { linenr, start_col - 1 } })
+		if node then
+			--- @type TSNode?
+			local curr = node
 
-						if #content_lines > 0 then
-							block_text = table.concat(content_lines, "\n")
-						end
+			while curr do
+				local type = curr:type()
 
-						break
+				if type:find("paragraph") or type:find("section") or type:find("list_item") then
+					local s_row, s_col, _, _ = curr:range()
+					final_start = { line = s_row, character = s_col }
+
+					local content_lines = {}
+					if not (s_row == linenr and s_col == start_col - 1) then
+						content_lines = vim.api.nvim_buf_get_text(bufnr, s_row, s_col, linenr, start_col - 1, {})
 					end
 
-					curr = curr:parent()
-				end
-			end
+					if #content_lines > 0 then
+						block_text = table.concat(content_lines, "\n")
+					end
 
-			table.insert(data.anchors, {
-				anchor_id = id,
-				content = block_text,
-				loc = {
-					start = final_start,
-					finish = { line = row, character = end_col - 1 },
-				},
-			})
+					break
+				end
+
+				curr = curr:parent()
+			end
 		end
+
+		table.insert(data.anchors, {
+			anchor_id = id,
+			content = block_text,
+			loc = {
+				start = final_start,
+				finish = { line = linenr, character = end_col - 1 },
+			},
+		})
 	end
 end
 
---- Parses links from the given lines and appends them to the data.
---- Links are expected to be of the form \[\[Note#Anchor|Alias\]\], where
---- each part is optional.
+--- Parses links from the line and appends them to the note. Links are expected
+--- to be of the form \[\[Note#Anchor|Alias\]\], where each part is optional.
 ---
---- @param lines string[] # The lines to parse
---- @param data BookwyrmNote # The parsed data
-local function parse_links(lines, data)
+--- @param linenr integer # The line number (0 indexed)
+--- @param line string # The line to parse
+--- @param data BookwyrmNote # The note to populate
+local function parse_links(linenr, line, data)
 	local id_pattern = "^[%w%-_]+$"
 
-	for i, line in ipairs(lines) do
-		for start_pos, raw_link, end_pos in line:gmatch("()%[%[(.-)%]%]()") do
-			local target, alias = raw_link:match("([^|]+)|?(.*)")
-			target = target or ""
+	for start_pos, raw_link, end_pos in line:gmatch("()%[%[(.-)%]%]()") do
+		local target, alias = raw_link:match("([^|]+)|?(.*)")
+		target = target or ""
 
-			local note, anchor = target:match("([^#]*)#?(.*)")
+		local note, anchor = target:match("([^#]*)#?(.*)")
 
-			note = vim.trim(note or "")
-			anchor = vim.trim(anchor or "")
+		note = vim.trim(note or "")
+		anchor = vim.trim(anchor or "")
 
-			if anchor ~= "" and not anchor:match(id_pattern) then
-				anchor = ""
-			end
+		if anchor ~= "" and not anchor:match(id_pattern) then
+			anchor = ""
+		end
 
-			table.insert(data.links, {
-				alias = (alias and alias ~= "") and vim.trim(alias) or nil,
-				context = line,
-				loc = {
-					start = { line = i - 1, character = start_pos - 1 },
-					finish = { line = i - 1, character = end_pos - 1 },
-				},
-				target_anchor = anchor ~= "" and anchor or nil,
-				target_note = note ~= "" and note or nil,
+		table.insert(data.links, {
+			alias = (alias and alias ~= "") and vim.trim(alias) or nil,
+			context = line,
+			loc = {
+				start = { line = linenr, character = start_pos - 1 },
+				finish = { line = linenr, character = end_pos - 1 },
+			},
+			target_anchor = anchor ~= "" and anchor or nil,
+			target_note = note ~= "" and note or nil,
+		})
+	end
+end
+
+--- Parses metadata from the note metadata
+---
+--- @param line string # The line in the metadata block
+--- @param data BookwyrmNote # The note to populate
+local function parse_metadata(line, data)
+	local title = line:match("^title:%s*(.+)$")
+	if title then
+		data.title = vim.trim(title):gsub("^([\"'])(.*)%1$", "%2")
+	end
+
+	local aliases = line:match("^alias:%s*%[?(.+)%]?(.*)$") or line:match("^aliases:%s*%[?(.+)%]?(.*)$")
+	if aliases then
+		for a in aliases:gmatch("([^,]+)") do
+			table.insert(data.aliases, {
+				alias = vim.trim(a):gsub("^([\"'])(.*)%1$", "%2"),
+			})
+		end
+	end
+
+	local tags = line:match("^tags:%s*%[?(.+)%]?(.*)$")
+	if tags then
+		for t in tags:gmatch("([^,]+)") do
+			table.insert(data.tags, {
+				tag = vim.trim(t):gsub("^#", ""),
 			})
 		end
 	end
@@ -215,9 +189,24 @@ function M.parse_buffer(bufnr)
 		title = vim.fn.fnamemodify(path, ":t:r"),
 	}
 
-	parse_metadata(lines, data)
-	parse_anchors(bufnr, lines, data)
-	parse_links(lines, data)
+	local inside_metadata = lines[1] == "---"
+	local start = inside_metadata and 2 or 1
+
+	local active_anchors = {}
+
+	for i = start, #lines do
+		local line = lines[i]
+		if inside_metadata and line == "---" then
+			inside_metadata = false
+		end
+
+		if inside_metadata then
+			parse_metadata(line, data)
+		else
+			parse_links(i, line, data)
+			parse_anchors(bufnr, i, line, active_anchors, data)
+		end
+	end
 
 	return data
 end
