c3c7e4c23bc057782058b311d6c6f1e44f950c63
Ed <ed.kilgore@tcnbroadcasting.com>
2026-02-01 17:48:05 -0500

    setup new state

--- lua/bookwyrm/state.lua
+++ lua/bookwyrm/state.lua
@@ -1,11 +1,26 @@
 --- @class BookwyrmState
 --- @field cfg BookwyrmConfig
 --- @field db BookwyrmDB?
+--- @field active_nb BookwyrmBook?
 local M = {}
 
 --- @class BookwyrmConfig
 --- @field data_path string
 --- @field db_path string
 --- @field silent boolean?
+
+--- Gets the current db connection.
+---
+--- @return BookwyrmDB
+function M.get_conn()
+	if not M.db then
+		M.db = require("bookwyrm.db").open(M.cfg.db_path, M.cfg.silent)
+		if not M.db then
+			error("Could not get db connection")
+		end
+	end
+
+	return M.db
+end
 
 return M
